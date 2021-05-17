4eef5f2325895caff683ebe113435e48233998d7
Roman Ihoshyn <igoshin18@gmail.com>
2021-05-17 16:54:40 +0300

    update script blade file

--- src/resources/views/script.blade.php
+++ src/resources/views/script.blade.php
@@ -3,7 +3,7 @@
 		@php($timeout = 1)
 		@foreach(array_reverse(session('flash_messages')) as $flash)
 	        setTimeout(() => {
-	            notify("{!! array_get($flash, 'message', '') !!}", '{{ array_get($flash, 'type', 'info') }}');
+	            window.notify("{!! $flash['message'] ?? '' !!}", '{{ $flash['type'] ?? 'info' }}');
 	        }, {{ $timeout }});
 		@php($timeout += config('flash-messages.timeout'))
 		@endforeach
